41c4b2267c9505a1dae2f4cab7a372d1ceb12156
Blah <blah@gmail.com>
2022-03-18 20:32:50 +0000

    added filtering movie list

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -8,6 +8,7 @@ import Grid from "@mui/material/Grid";
 const MovieListPage = (props) => {
   const [movies, setMovies] = useState([]);
 
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -22,6 +23,24 @@ const MovieListPage = (props) => {
       });
   }, []);
 
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+
+  const genreId = Number(genreFilter);
+
+  let displayedMovies = movies
+    .filter((m) => {
+      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
+
   return (
     <Grid container sx={{ padding: "20px" }}>
       <Grid item xs={12}>
@@ -29,9 +48,13 @@ const MovieListPage = (props) => {
       </Grid>
       <Grid item container spacing={5}>
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <FilterCard />
+        <FilterCard
+      onUserInput={handleChange}
+      titleFilter={nameFilter}
+      genreFilter={genreFilter}
+    />
         </Grid>
-        <MovieList movies={movies}></MovieList>
+        <MovieList movies={displayedMovies} />
       </Grid>
     </Grid>
   );
