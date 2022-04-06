84ac0deeac5977df749682c0dc31bbb2f7d4fb2b
Blah <blah@gmail.com>
2022-04-06 19:03:00 +0100

    fingers crossed

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -25,6 +25,21 @@ export const getMovies = () => {
        throw error
     });
   };
+
+  export const getPopularMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
+  };
+
   
   export const getMovie = (args) => {
     // console.log(args)
